4efc921a75c5e15d9cab81a6c8a656695d5f3e77
MrPalmaBR <luizzp@msn.com>
2023-10-31 08:04:42 -0300

    Atualização do Código

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 // Mensagem de apresentação do projeto
-console.log("Seja bem-vindo (a) ao Classificador de Heróis");
+console.log("Seja bem-vindo (a) à Calculadora de Partidas Rankeadas!");
 console.log("");
 
-// Entrada do usuário, com os dados do herói
-let nomeHeroi = "Tonhão";
-let xp = 1001;
+// Criação da função para calcular o rank
+function calcularRank(numVitorias,numDerrotas){
 
-// Matriz de níveis, onde cada submatriz contém [nome do nível, limite de XP]
-const niveis = [
-    ["Ferro" , 1000],
-    ["Bronze" , 2000],
-    ["Prata" , 5000],
-    ["Ouro" , 7000],
-    ["Platina" , 8000],
-    ["Ascendente" , 9000],
-    ["Imortal" , 10000],
-];
+	// Calculadora do saldo do jogador:
+	let saldo = numVitorias - numDerrotas;
 
-// Valor padrão quando o nível declarado não estiver dentro da matriz
-let nivel = "Radiante";
+	// Matriz do ranking, onde cada submatriz contém [nome do 	rank, saldo de rankeadas]
+	const ranks = [
+    	["Ferro" , 10],
+    	["Bronze" , 20],
+    	["Prata" , 50],
+    	["Ouro" , 80],
+    	["Diamante" , 90],
+    	["Lendário" , 100],
+	];
 
-// Atribui o nome do nível e o limite de XP da matriz 'niveis' às variáveis correspondentes
-for (let i = 0; i < niveis.length; i++) {
-    const nomeNivel = niveis[i][0];
-    const limiteXP = niveis[i][1];
+	// Valor padrão quando o saldo declarado não estiver dentro da matriz
+	let nomeRank = "Imortal";
 
-    // Quando encontramos o nível adequado, podemos sair do loop
-    if (xp <= limiteXP) {
-        nivel = nomeNivel;
-        break;
-    }
+	// Atribui o nome do reank e o limite de saldo da matriz 'ranks' às variáveis correspondentes
+	for (let i = 0; i < ranks.length; i++) {
+    	const nomeRankAtual = ranks[i][0];
+    	const limiteSaldo = ranks[i][1];
+
+    	// Quando encontramos o saldo adequado, podemos sair do loop
+    	if (saldo <= limiteSaldo) {
+        	nomeRank = nomeRankAtual;
+        	break;
+    	}
+	}
+	// Retornando os resultados
+    return {saldo, nomeRank};
 }
 
+// Entrada do usuário, com os dados do jogador
+let nicknameJogador = "Tonhão";
+let numVitorias = 21;
+let numDerrotas = 50;
+
+// Chamando a função para informar o rank
+let resultado = calcularRank(numVitorias,numDerrotas);
+
 // Resultado final
-console.log("O herói de nome " + nomeHeroi + " está no nível de " + nivel);
+console.log(`O jogador ${nicknameJogador} tem um herói com um saldo de ${resultado.saldo} vitórias está no nível de ${resultado.nomeRank}`);
